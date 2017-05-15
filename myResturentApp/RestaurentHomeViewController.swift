2173bde56215c76119e49cd52f61231b3833dfaa
Erozone <Erozone@Mohits-Mac-mini.local>
2017-05-15 22:04:41 +0530

    Implementing Core Location in Restaurent Home

---
+++ myResturentApp/RestaurentHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurentHomeViewController.swift
+//  myResturentApp
+//
+//  Created by Mohit Kumar on 15/05/17.
+//  Copyright © 2017 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class RestaurentHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
